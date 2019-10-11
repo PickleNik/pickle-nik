bea7468cd1e63504997ab3fcee9c138958c69539
PickleNik <krupin.nikita0110@gmail.com>
2019-10-11 19:08:14 -0400

    commit

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,16 @@
         <img src="https://www.upwork.com/profile-portraits/c18GQ_SazI15TjBtgsPQevIocCzc7uI0SFZvk3saSug-1lVso7zW6rbpyCqWaV7uj0?1557003397602">
       </v-avatar>
       <i>Nikita K.</i></h1>
-      <p><v-icon large class="hidden-sm-and-down white--text mb-1 mr-2">location_on</v-icon><v-icon class="hidden-md-and-up mr-1">location_on</v-icon>Rockville, MD</p>
+      <p><b class="pink--text">at</b> Rockville, MD</p>
     </v-flex>
 
     <v-flex md4 lg3 class="pa-3 hidden-sm-and-down" style="font-size: 2em; position: fixed; bottom:0; right: 0; z-index: 1;">
       <v-layout column>
-        <v-btn block round flat class="link link--kukuri" v-scroll-to="{el:'#info', offset: 0}"><v-spacer></v-spacer>Info</v-btn>
-        <v-btn block round flat class="link link--kukuri" v-scroll-to="{el:'#work', offset: 0}"><v-spacer></v-spacer>Work</v-btn>
-        <v-btn block round flat class="link link--kukuri" v-scroll-to="{el:'#contact', offset: 0}"><v-spacer></v-spacer>Contact</v-btn>
-        <v-btn block round flat class="link link--kukuri" v-scroll-to="{el:'#picture', offset: 0}"><v-spacer></v-spacer>Picture</v-btn>
+        <v-btn block round flat class="my-3 link link--kukuri" v-scroll-to="{el:'#info', offset: 0}"><v-spacer></v-spacer>Info <v-icon right>home</v-icon></v-btn>
+        <v-btn block round flat class="my-3 link link--kukuri" v-scroll-to="{el:'#work', offset: 0}"><v-spacer></v-spacer>Work <v-icon right>web</v-icon></v-btn>
+        <v-btn block round flat class="my-3 link link--kukuri" v-scroll-to="{el:'#skills', offset: 0}"><v-spacer></v-spacer>Skills <v-icon right>done_all</v-icon></v-btn>
+        <v-btn block round flat class="my-3 link link--kukuri" v-scroll-to="{el:'#picture', offset: 0}"><v-spacer></v-spacer>Picture <v-icon right>photo</v-icon></v-btn>
+        <v-btn block round flat class="my-3 link link--kukuri" v-scroll-to="{el:'#contact', offset: 0}"><v-spacer></v-spacer>Contact <v-icon right>pets</v-icon></v-btn>
       </v-layout>
     </v-flex>
 
@@ -26,6 +27,8 @@
     <v-layout column id="info" class="height" style="background: -webkit-linear-gradient(to right, #232526, #414345); z-index: 0;">
       <span class="hidden-sm-and-down my-auto mx-auto" style="font-size: 3em; text-shadow: 0 0 1em #000">Fullstack
         <h1 style="z-index: 1">Web Developer</h1>
+        <v-layout row><v-spacer></v-spacer><span><b class="pink--text">&</b> UI/UX, Graphic & Motion</span></v-layout>
+        <v-layout row><v-spacer></v-spacer><h1 style="z-index: 1;">Designer</h1></v-layout>
       </span>
       <h1 style="z-index: 1; font-size: 2em;  text-shadow: 0 0 1em white; text-align: center;" class="hidden-md-and-up my-auto mx-auto">Fullstack Web Developer</h1>
     </v-layout>
@@ -111,10 +114,240 @@
       </v-flex>
     </v-flex>
 
-    <v-flex class="height" id="contact">
+    <v-flex class="height-2" id="skills">
       <v-flex class="pa-5" style="font-size: 2em; position: absolute; top: 600vh; left: 0; z-index: 1;">
+        <h1><v-icon size="60" left class="hidden-sm-and-down mb-2 pink--text">done_all</v-icon><v-icon size="30" class="hidden-md-and-up mb-1 mr-1 pink--text">pets</v-icon>Skills</h1>
+        <p>Code like a pro</p>
+      </v-flex>
+      <v-layout row wrap style="margin-top: 200px;">
+        <v-flex xs12 md5 class="pa-5 text-xs-right">
+          <h1 style="font-size: 3em;" class="my-3">Frontend</h1>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 orange--text">HTML5</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="orange" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 blue--text">CSS3</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="blue" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 yellow--text">JS</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="yellow" value="90"><h2 class="black--text pl-3">90%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 amber--text">ES6</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="amber" value="90"><h2 class="black--text pl-3">90%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 pink--text">Sass</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="pink" value="50"><h2 class="black--text pl-3">50%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 blue--text">React.js</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="blue" value="50"><h2 class="black--text pl-3">50%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 teal--text">Vue.js</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="teal" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 cyan--text">Electron</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="cyan" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+        <v-flex xs12 md5 class="pa-5 text-xs-right">
+          <h1 style="font-size: 3em;" class="my-3">Backend</h1>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 orange--text">Firebase</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="orange" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 green--text">Node.js</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="green" value="90"><h2 class="black--text pl-3">90%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 red--text">Axios</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="red" value="40"><h2 class="black--text pl-3">40%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 purple--text">Linux</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="purple" value="40"><h2 class="black--text pl-3">40%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 indigo--text">AWS</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="indigo" value="40"><h2 class="black--text pl-3">40%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+        <v-flex xs12 md5 class="pa-5 text-xs-right">
+          <h1 style="font-size: 3em;" class="my-3">Basic</h1>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 red--text">G-Suite</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="red" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 orange--text">Power Point</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="orange" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 amber--text">G-Slides</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="amber" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 green--text">Excel</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="green" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 green--text">G-Sheets</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="green" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 blue--text">Word</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="blue" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 blue--text">G-Docs</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="blue" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 cyan--text">Outlook</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="cyan" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+        <v-flex xs12 md5 class="pa-5 text-xs-right">
+          <h1 style="font-size: 3em;" class="my-3">Other</h1>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 pink--text">Slack</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="pink" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 blue--text">Trello</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="blue" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 blue--text">Jira</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="blue" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 blue--text">Bitbucket</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="blue" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 grey--text">Github</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="grey" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 orange--text">Git</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="orange" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="my-3">
+            <v-spacer></v-spacer>
+            <h1 class="pt-2 green--text">Vectr</h1>
+            <v-flex xs10 md6>
+              <v-progress-linear class="round2 mx-3 text-xs-left" height="2.4em" color="green" value="100"><h2 class="black--text pl-3">100%</h2></v-progress-linear>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-flex>
+
+    <v-flex class="height pa-5" id="picture">
+      <v-flex class="pa-5" style="font-size: 2em; position: absolute; top: 800vh; left: 0; z-index: 1;">
+        <h1><v-icon size="60" left class="hidden-sm-and-down mb-2 pink--text">photo</v-icon><v-icon size="30" class="hidden-md-and-up mb-1 mr-1 pink--text">pets</v-icon>Picture</h1>
+        <p>National Geographic</p>
+      </v-flex>
+
+      <v-flex xs12 md10 offset-md1 lg6 offset-lg3 style="margin-top: 50vh; transform: translateY(-50%);">
+        <v-img class="tilt hidden-sm-and-down round2 mx-auto" contain aspect-raito="1" src="ngnikita.jpg" style="max-height: 70vh;">
+        </v-img>
+        <v-img v-ripple class="hidden-md-and-up round2 mx-auto" contain aspect-raito="1.7888" src="ngnikita.jpg" style="max-height: 70vh;"></v-img>
+      </v-flex>
+    </v-flex>
+
+    <v-flex class="height" id="contact">
+      <v-flex class="pa-5" style="font-size: 2em; position: absolute; top: 900vh; left: 0; z-index: 1;">
         <h1><v-icon size="60" left class="hidden-sm-and-down mb-2 pink--text">pets</v-icon><v-icon size="30" class="hidden-md-and-up mb-1 mr-1 pink--text">pets</v-icon>Contact</h1>
-        <p>Open to offers</p>
+        <p>Currently busy, but checking the email :)</p>
       </v-flex>
 
       <v-layout row wrap class="hidden-sm-and-down" style="margin-top: 50vh; transform: translateY(-50%);">
@@ -134,6 +367,10 @@
           </v-avatar>
         </a>
       </v-layout>
+      <v-flex class="pa-5" style="font-size: 2em; position: absolute; top: calc(1000vh - 160px); left: 0; z-index: 1;">
+        <p><v-icon class="mr-1 pink--text" large>email</v-icon>krupin.nikita@gmail.com, <v-icon class="mr-1 pink--text" large>phone</v-icon>+1(240)716-4663</p>
+      </v-flex>
+
       <v-layout column class="hidden-md-and-up" style="margin-top: 50vh; transform: translateY(-50%);">
         <a target="_blank" class="mx-auto my-3" href="https://www.linkedin.com/in/picklenik/">
           <v-avatar v-ripple size="100" class="white">
@@ -151,19 +388,6 @@
           </v-avatar>
         </a>
       </v-layout>
-    </v-flex>
-
-    <v-flex class="height pa-5" id="picture">
-      <v-flex class="pa-5" style="font-size: 2em; position: absolute; top: 700vh; left: 0; z-index: 1;">
-        <h1><v-icon size="60" left class="hidden-sm-and-down mb-2 pink--text">photo</v-icon><v-icon size="30" class="hidden-md-and-up mb-1 mr-1 pink--text">pets</v-icon>Picture</h1>
-        <p>National Geographic</p>
-      </v-flex>
-
-      <v-flex xs12 md10 offset-md1 lg6 offset-lg3 style="margin-top: 50vh; transform: translateY(-50%);">
-        <v-img class="tilt hidden-sm-and-down round2 mx-auto" contain aspect-raito="1" src="ngnikita.jpg" style="max-height: 70vh;">
-        </v-img>
-        <v-img v-ripple class="hidden-md-and-up round2 mx-auto" contain aspect-raito="1.7888" src="ngnikita.jpg" style="max-height: 70vh;"></v-img>
-      </v-flex>
     </v-flex>
 
   </v-app>
@@ -215,11 +439,21 @@ export default {
       font-size: 0.6em;
     }
   }
+  .bg {
+    background: url('https://images.unsplash.com/photo-1506318137071-a8e063b4bec0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1970&q=80') fixed no-repeat !important;
+    background-color: black !important;
+    background-repeat: no-repeat !important;
+    background-size: cover !important;
+    background-position: fixed !important;
+  }
   ::-webkit-scrollbar{
     width: 0;
   }
   .height {
     height: 100vh;
+  }
+  .height-2 {
+    height: 200vh;
   }
   .round1 {
     border-radius: 1em;
